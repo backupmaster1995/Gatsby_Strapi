5a4ef55527f738da6e53a4a1bf62484fc1442ada
backupmaster1995 <86629904+backupmaster1995@users.noreply.github.com>
2021-08-06 21:51:42 -0300

    Add files via upload

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a52dcfd97f2c46f21405c467624d09d1'),
+    },
+  },
+});
